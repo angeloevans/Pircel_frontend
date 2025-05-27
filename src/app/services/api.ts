2d9cf7c1cc8de02000a549bacd3bd61dcb13b5b1
Angelo Evangelopoulos <135064484+angeloevans@users.noreply.github.com>
2025-05-27 17:59:49 +0300

    Update api.ts
    Changing the API to the given one

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,7 +1,8 @@
 import { House } from "../components/HouseCard";
 
 export async function fetchHouses(): Promise<House[]> {
-  const res = await fetch("http://localhost:5000/houses");
+  // const res = await fetch("http://localhost:5000/houses"); for local testing
+  const res = await fetch("https://wizard-world-api.herokuapp.com/houses");  // This would change to deployed Backend API
   if (!res.ok) {
     throw new Error("Failed to fetch houses");
   }
